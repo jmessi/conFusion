8ca3a63536e4380002b9ec742196ee30c7422d27
Joao Messias <joaopedro.miguelmessias@altran.com>
2021-05-31 16:15:07 +0100

    Components Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { MatListModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 import 'hammerjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
