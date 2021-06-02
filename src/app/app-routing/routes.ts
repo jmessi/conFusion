a3fd60331f5cf7d7730538634afbcd05aab62a3c
Joao Messias <joaopedro.miguelmessias@altran.com>
2021-06-02 17:19:40 +0100

    SPA Part 1

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
-import { MenuComponent } from '../menu/menu.component';
 import { HomeComponent } from '../home/home.component';
+import { MenuComponent } from '../menu/menu.component';
 import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
 //import { DishdetailComponent } from '../dishdetail/dishdetail.component';
@@ -10,6 +10,6 @@ export const routes: Routes =[
   { path: 'home', component: HomeComponent },
   { path: 'menu', component: MenuComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
+  { path: 'contactus', component: ContactComponent },
   { path:'', redirectTo: '/home', pathMatch:'full'}
 ];
