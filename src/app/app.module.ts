385fc5eaaf6d8043256a9da8424401fc6d6d04a5
Joao Messias <joaopedro.miguelmessias@altran.com>
2021-06-02 16:13:39 +0100

    Basic Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
-import { DishdetailComponent } from './dishdetail/dishdetail.component';
-import { MenuComponent } from './menu/menu.component';
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service';
 
+import { AppRoutingModule } from "./app-routing/app-routing.module";
 
 @NgModule({
   declarations: [
@@ -26,13 +30,17 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
+    AppRoutingModule,
     MatListModule,
     MatGridListModule,
     MatCardModule,
