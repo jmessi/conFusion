e8ead2bc30e6bc403a80596a71236c2a66f08a76
Joao Messias <joaopedro.miguelmessias@altran.com>
2021-06-11 10:11:02 +0100

    Promise Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -20,8 +20,9 @@ export class DishdetailComponent implements OnInit {
     private route: ActivatedRoute ) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.params['id'];
-    this.dish = this.dishService.getDish(id);
+    const id = this.route.snapshot.params['id'];
+    this.dishService.getDish(id)
+      .then((dish) => this.dish = dish);
   }
 
   goBack(): void{
